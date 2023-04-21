36a690d2ab82fd90e33d459e0a1dc7cad7f23ce7
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2023-04-21 12:16:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "9302cd0cff1e2aa81379d95aeba7f27b")
     BOT_TOKEN = getenv("BOT_TOKEN", "6078472433:AAE9iX1kyC9NNCJOBDivaPsi9QiNXEa_Sp8")
     FSUB = getenv("FSUB", "Hackinsider")
-    CHID = int(getenv("CHID", "-1928008250"))
+    CHID = int(getenv("CHID", "-1001928008250"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://zedteam21:PuvA0Slfpvu44OvS@cluster0.bzwi3j4.mongodb.net/?retryWrites=true&w=majority")
     
