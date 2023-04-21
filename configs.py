ad25c44aa6c11f6bd6ebeffa614c6fa37a024add
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2023-04-21 11:55:38 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
+    API_ID = int(getenv("API_ID", "1881742"))
+    API_HASH = getenv("API_HASH", "9302cd0cff1e2aa81379d95aeba7f27b")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6078472433:AAE9iX1kyC9NNCJOBDivaPsi9QiNXEa_Sp8")
+    FSUB = getenv("FSUB", "Hackinsider")
+    CHID = int(getenv("CHID", "-1928008250"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://zedteam21:PuvA0Slfpvu44OvS@cluster0.bzwi3j4.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
